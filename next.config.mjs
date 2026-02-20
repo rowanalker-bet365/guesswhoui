e84b88b60dae5c6bc125a9a763b48a12e3e35a53
Rowan Alker <rowanalker@bet365.com>
2026-02-20 19:17:18 +0000

    deploy-02: seperated server side dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,17 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        child_process: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
