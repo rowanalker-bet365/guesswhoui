e12bdbde9d7fe28f26a28aef82830820755cfa80
Rowan Alker <rowanalker@bet365.com>
2026-02-26 15:10:56 +0000

    styling-fixes: nav button fixes

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -17,6 +17,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        nav: "h-auto rounded-none p-0 text-sm font-medium text-white underline-offset-2 hover:underline focus:outline-none",
       },
       size: {
         default: "h-10 px-4 py-2",
